b5143cb2f903a79adb6ab6af5f6d43788bf6338d
murali1466 <muralikrishnatangella1466@gmail.com>
2024-07-03 17:26:47 +0530

    issue

--- src/content/HeroContent/page.js
+++ src/content/HeroContent/page.js
@@ -20,18 +20,13 @@ const HeroContent = () => {
     >
       <Container maxWidth="md">
         <Typography variant="h2" component="h1" gutterBottom>
-          Welcome to Lola's Unisex-Family Salon
+         {"Welcome to Lola's Unisex-Family Salon"}
         </Typography>
         <Typography variant="h5" component="p" gutterBottom>
           Where Beauty Meets Comfort
         </Typography>
         <Typography variant="body1" component="p" gutterBottom>
-          At Lola's Unisex-Family Salon, we believe that everyone deserves to
-          look and feel their best. Our team of expert stylists and beauticians
-          are dedicated to providing top-notch services for the entire family.
-          From trendy haircuts and stylish coloring to relaxing facials and
-          rejuvenating treatments, we offer a comprehensive range of services
-          tailored to meet your unique needs.
+         {"At Lola's Unisex-Family Salon, we believe that everyone deserves"}
         </Typography>
         <Button variant="contained" color="primary" size="large">
           Book Your Appointment Today!
